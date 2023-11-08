56c6bc0a372ea892995efee9b0cd7f210185309b
tutulhaque <sdtutulctg@gmail.com>
2023-11-08 15:46:32 +0600

    Error page Created

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -1,10 +1,22 @@
-
+import { Link } from "react-router-dom";
+import img404 from '../assets/404-status-code.png'
+import { Helmet } from "react-helmet";
 
 
 const Error = () => {
     return (
         <div>
-            
+            <Helmet>
+                <title>Error</title>
+            </Helmet>
+            <div className="h-screen py-28">
+                <div className="flex justify-center items-center">
+                    <img className="w-96" src={img404} alt="" />
+                </div>
+                <div className="flex justify-center items-center py-8">
+                    <Link to='/'><button className="btn btn-neutral text-white">Back To Home</button></Link>
+                </div>
+            </div>
         </div>
 
     );
