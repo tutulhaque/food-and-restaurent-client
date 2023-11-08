5e271742ad37135b9e88d68b381a2da62c463396
tutulhaque <sdtutulctg@gmail.com>
2023-11-08 15:28:38 +0600

    Data added to Home Page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,25 @@
-import { Helmet } from 'react-helmet';
+import { useContext, useState } from "react";
+import { AuthContext } from "../../Component/Providers/AuthProvider";
+import Banner from "../../Component/Header/Banner";
+import Testimonial from "./Testimonial";
+import { useLoaderData } from "react-router-dom";
+import FoodsByCount from "../../Component/Food/FoodsByCount";
+import Gallery from "./Gallery";
 
 const Home = () => {
-
+    const authInfo = useContext(AuthContext);
+    console.log(authInfo)
+    const brandLoadedData = useLoaderData();
+    // const [brands, setBrands] = useState(brandLoadedData);
+    console.log(brandLoadedData);
     return (
         <div>
+            <Banner></Banner>
+            <FoodsByCount></FoodsByCount>
+            <Gallery></Gallery>
+            <Testimonial></Testimonial>
+
+
         </div>
     );
 };
