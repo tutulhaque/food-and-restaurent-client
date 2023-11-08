49d7353da626cda9f24986b16447db570f17ed98
tutulhaque <sdtutulctg@gmail.com>
2023-11-08 15:37:23 +0600

    Register and Login Funtionality implemented

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,25 +1,26 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { AuthContext } from "../../Component/Providers/AuthProvider";
 import Banner from "../../Component/Header/Banner";
 import Testimonial from "./Testimonial";
 import { useLoaderData } from "react-router-dom";
 import FoodsByCount from "../../Component/Food/FoodsByCount";
 import Gallery from "./Gallery";
+import { Helmet } from "react-helmet";
 
 const Home = () => {
     const authInfo = useContext(AuthContext);
     console.log(authInfo)
     const brandLoadedData = useLoaderData();
-    // const [brands, setBrands] = useState(brandLoadedData);
     console.log(brandLoadedData);
     return (
         <div>
+            <Helmet>
+                <title>Home</title>
+            </Helmet>
             <Banner></Banner>
             <FoodsByCount></FoodsByCount>
             <Gallery></Gallery>
             <Testimonial></Testimonial>
-
-
         </div>
     );
 };
